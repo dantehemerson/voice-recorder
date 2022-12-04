838ece863db6e619d0902884d602417e95333b72
Dante Calderón <dantehemerson@gmail.com>
2022-12-04 00:57:39 -0500

    feat: Change project structure (#1)
    * change project structure
    
    * Some updates
    
    * Updates
    
    * Remove unused projects

--- apps/frontend-e2e/src/e2e/RecordButton--RecordButton/RecordButton.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('frontend: RecordButton component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=recordbutton--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to RecordButton!');
-    });
-});
