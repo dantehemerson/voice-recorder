6e63d21ed7a7d88054013e355e9b88db0de7b911
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-05-27 18:49:00 -0500

    florian broke the regex, lame

--- apps/frontend/fonts/index.ts
+++ apps/frontend/fonts/index.ts
@@ -1,4 +1,13 @@
 import { Montserrat, Chivo } from '@next/font/google';
+import localFont from '@next/font/local';
 
 export const MainFont = Montserrat({ subsets: ['latin'] });
 export const MonoFont = Chivo({ subsets: ['latin'] });
+export const nexa = localFont({
+  src: [
+    { path: './Nexa-Light.otf', weight: 'normal' },
+    { path: './Nexa-Bold.otf', weight: 'bold' },
+  ],
+});
+
+console.log('Nexa', nexa);
