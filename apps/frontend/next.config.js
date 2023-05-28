1d5432dd52195883f81ce91b33eef7cc6b3034e3
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-05-27 20:56:31 -0500

    these guys are flipped

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,6 +7,14 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3333/api/:path*',
+      },
+    ];
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
