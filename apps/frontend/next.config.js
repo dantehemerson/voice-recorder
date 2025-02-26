eb6628c0c68a5c14294e00eeb43db3c30bae3a71
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2025-02-26 01:24:52 -0500

    Update

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
      *  { "ssr": true, "displayName": true, "preprocess": false }
      */
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
